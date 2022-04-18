330b2144e992ad4b69495844454a5125db5ecfb5
Tapolcsanyi <iceike237@gmail.com>
2022-04-17 19:30:05 -0500

    maintenance

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+//npm start
+//json-server -p 8088 -w database.json
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
